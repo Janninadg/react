14a56f822e059fb2937342cd342a528732a4cdd0
Janninadg <jannina.delosgodos@pucp.edu.pe>
2023-02-25 16:03:06 -0500

    changes project

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,73 @@
+import react, {Component} from 'react';
+import { Card, CardImg,CardText,CardImgOverlay, Media, CardTitle, CardBody } from 'reactstrap';
+
+
+
+class Menu extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedAnimal : null
+        }
+
+        console.log("Menu componet constructor is invoked");
+    }
+
+    componentDidMount(){
+        console.log("Menu componetDidMount is invoked");
+    }
+
+    onAnimalSelected(animal){
+        this.setState({selectedAnimal:animal});
+    }
+
+    renderAnimal(animal){
+        if(animal != null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={animal.image} alt={animal.name} />
+                    <CardBody>
+                        <CardTitle>{animal.name}</CardTitle>
+                        <CardText>{animal.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        } else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    render(){
+
+        const menu = this.props.animals.map((animal) => {
+            return(
+                <div key={animal.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onAnimalSelected(animal)}>
+                        <CardImg width="100%" src={animal.image} alt={animal.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{animal.name}</CardTitle>
+                            <CardText>{animal.description}</CardText>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className='container'>
+                <div className='row'>
+                        {menu}
+                </div>
+                <div className='row'>
+                    {this.renderAnimal(this.state.selectedAnimal)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
---
+++ src/shared/animals.js
@@ -0,0 +1,26 @@
+export const ANIMALS = [
+    {
+        id:0,
+        name:'Pez',
+        image:'assets/images/pez.jpg',
+        description:'Vive en el mar'
+    },
+    {
+        id:1,
+        name:'Leon',
+        image:'assets/images/leon.jpg',
+        description:'Animal salvaje'
+    },
+    {
+        id:2,
+        name:'Perro',
+        image:'assets/images/perro.jpg',
+        description:'Animal doméstico, canino'
+    },
+    {
+        id:3,
+        name:'Gato',
+        image:'assets/images/gato.jpg',
+        description:'Animal doméstico, felino'
+    }
+]
